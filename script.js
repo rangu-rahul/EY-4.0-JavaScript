2a503a21bf473743c0f4aeff45ade09122ea94ad
rahul <rangurahul98@gmail.com>
2025-09-22 10:26:54 +0530

    variables

---
+++ script.js
@@ -0,0 +1,20 @@
+// var a = 10;
+// console.log(a);
+
+
+// var a = 10;
+// var b= 68;
+// console.log(b);
+// console.log(`The value b is : ${b}`);
+
+let a = 10;
+const c = 20;
+const c = 30;
+console.log(`the value is ${a}`);
+
+
+
+
+
+
+
