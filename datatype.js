08e4f499c2727866d336f0a0e4914c6421e0a4a0
rahul <rangurahul98@gmail.com>
2025-09-22 11:02:43 +0530

    arrays

---
+++ datatype.js
@@ -0,0 +1,30 @@
+
+ f = 60;
+ console.log(typeof(f));
+
+
+ name = "Rahul";
+ console.log(typeof(name));
+
+ c = true;
+ console.log(typeof(c));
+
+    n = null;
+    console.log(typeof(n));
+
+    d = undefined;
+    console.log(typeof(d));
+
+
+    //arrays
+    array = [20,"Rahul",true,null];
+    console.log(typeof(array));
+    console.log(array[2]);
+    obj = {
+        namee : "Rahul",
+        age : 20,
+        dept : "AIML"
+    };
+    console.log(obj);
+    console.log(typeof(obj));
+
--- script.js
+++ script.js
@@ -9,12 +9,20 @@
 
 let a = 10;
 const c = 20;
-const c = 30;
-console.log(`the value is ${a}`);
+const d = 30;
+console.log(`the value is ${c}`);
 
 
+// data datatypes
+// 1.primitive datatype
+//number,boolean,null,string,undefined,symbol
+
+// 2.non-primitive datatype
+//object,array,function
+//typeof() - is used to know the type of the datatype
+//this,window -- commands to display function of the js
 
 
-
-
+ f = 60;
+ console.log(typeof(f));
 
