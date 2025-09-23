9ba6f17b54ecd159af20852f96b963c276788d35
rahul <rangurahul98@gmail.com>
2025-09-23 10:54:47 +0530

    flex and grids

---
+++ conditional.js
@@ -0,0 +1,15 @@
+// a = Number(prompt("Enter the age :"));
+// if(a  < 18){
+//     document.write("You are not Eligible to vote");
+
+// }else{
+//     document.write("You are Eligible to vote");
+// }
+
+a = String(prompt("Enter the age :"));
+if(a  < '18'){
+    document.write("You are not Eligible to vote");
+
+}else{
+    document.write("You are Eligible to vote");
+}
